bbc8f8bbfcc7ea504e800b39836149bae3390f5e
gopalregar0757 <gopal0779t@gmail.com>
2025-05-31 15:53:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+intents.message_content = True  # REQUIRED for commands
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -15,9 +14,5 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("🏓 Pong!")
-
-@bot.command()
-async def say(ctx, *, message):
-    await ctx.send(message)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
